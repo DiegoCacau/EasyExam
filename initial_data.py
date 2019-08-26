27081be1a5fa69c2d3c0530956aedf4b301c7e9e
Diego Cacau <dc_cacau@hotmail.com>
2019-08-25 23:33:25 -0300

    master: adding fixtures

--- initial_data.py
+++ initial_data.py
@@ -6,4 +6,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "es_project.settings")
 django.setup()
 
 call_command('loaddata', 'sex', app_label='dashboard')
-
+call_command('loaddata', 'motivations', app_label='dashboard')
+call_command('loaddata', 'hdas', app_label='dashboard')
+call_command('loaddata', 'levels', app_label='dashboard')
+call_command('loaddata', 'hpps', app_label='dashboard')
